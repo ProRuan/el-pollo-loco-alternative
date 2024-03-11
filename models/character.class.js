7c23e7170fd9ebd621db22cb11558473f4e50163
Your Name <you@example.com>
2024-03-11 22:20:35 +0100

    created loadImagesWalking function

--- models/character.class.js
+++ models/character.class.js
@@ -3,21 +3,45 @@ class Character extends MoveableObject {
     height = this.width;
     x = 32;
     y = 540 - 128 - 32;
-    speed = 10;
+    speed = 4;
+    IMAGES_WALKING_PATH = 'img/characters/knight/Walk/walk';
+    IMAGES_WALKING_AMOUNT = 6;
+    IMAGES_WALKING = this.loadImagesWalking(this.IMAGES_WALKING_PATH, this.IMAGES_WALKING_AMOUNT);
     world;
 
 
     constructor() {
         super().loadImage('img/characters/knight/knight.png');
+        this.loadImages(this.IMAGES_WALKING);
         this.animate();
     }
 
 
     animate() {
         setInterval(() => {
+            if (this.world.keyboard.LEFT.value) {
+                this.moveLeft();
+            }
             if (this.world.keyboard.RIGHT.value) {
                 this.moveRight();
             }
         }, 1000 / 60);
+
+        setInterval(() => {
+            if (this.world.keyboard.RIGHT.value || this.world.keyboard.LEFT.value) {
+                this.playAnimation(this.IMAGES_WALKING);
+            }
+        }, 1000 / 60 * 6);    // set value!!! + array.length!!!
+    }
+
+
+    loadImagesWalking(path, amount) {
+        let images = [];
+        for (let i = 0; i < amount; i++) {
+            let serial = i + 1;
+            let img = path + serial + '.png';
+            images.push(img);
+        }
+        return images;
     }
 }
--- models/moveable-object.js
+++ models/moveable-object.js
@@ -6,8 +6,21 @@ class MoveableObject extends DrawableObject {
     energy = 100;
     lastHit = 0;    // to rename?
 
-    
+
+    moveLeft() {
+        this.x -= this.speed;
+    }
+
+
     moveRight() {
         this.x += this.speed;
+    }
+
+
+    playAnimation(images) {
+        let i = this.currentImage % images.length;
+        let path = images[i];
+        this.img = this.imageCache[path];
+        this.currentImage++;
     }
 }
