ad27f88876ed01ad86fbd7ade30aa5153df15214
Your Name <you@example.com>
2024-03-16 21:17:33 +0100

    add enemy attack range

--- models/drawable-objects.class.js
+++ models/drawable-objects.class.js
@@ -45,9 +45,18 @@ class DrawableObject {
             ctx.beginPath();
             ctx.lineWidth = '2';
             ctx.strokeStyle = 'red';
-            // ctx.rect(this.x + 60, this.y + 60, 48, 48);    // attack: x + 60, y + 60, 48, 48
+            ctx.rect(this.x + 60, this.y + 60, 48, 48);    // attack: x + 60, y + 60, 48, 48
             // ctx.rect(this.x + 60, this.y + 60, 36, 48);    // walk atttack, run attack: x + 60, y + 60, 36, 48
-            ctx.rect(this.x + 60, this.y + 32, 48, 80);    // attack: x + 60, y + 60, 48, 48
+            // ctx.rect(this.x + 60, this.y + 32, 48, 80);    // attack: x + 60, y + 60, 48, 48
+            ctx.stroke();
+        }
+
+        if (this instanceof Enemy) {
+            ctx.beginPath();
+            ctx.lineWidth = '2';
+            ctx.strokeStyle = 'purple';
+            // ctx.rect(this.x, this.y + 40, 104, 48);    // x, y + 40, 104, 48
+            ctx.rect(this.x + 84, this.y + 40, 40, 40);    // x + 84, y + 40, 40, 40
             ctx.stroke();
         }
 
--- models/enemy.class.js
+++ models/enemy.class.js
@@ -1,15 +1,17 @@
 class Enemy extends MoveableObject {
     width = 128;
     height = this.width;
-    x = 960;
+    x = 640;
     y = 540 - this.height - 16;
     startTime = new Date().getTime();
+    // speed = 0;    // for testing
     speed = 64 / 60;
+    // otherDirection = false;    // for testing
     otherDirection = true;
     isBeated = false;
     FLIP_BOOK = new FlipBook();
-    FLIP_BOOK_ATTACK = this.FLIP_BOOK.attack;
-    FLIP_BOOK_DEATH = this.FLIP_BOOK.death;
+    FLIP_BOOK_ATTACK = this.FLIP_BOOK.dinoAttack;
+    FLIP_BOOK_DEATH = this.FLIP_BOOK.dinoDeath;
     FLIP_BOOK_HURT = this.FLIP_BOOK.dinoHurt;
     FLIP_BOOK_IDLE = this.FLIP_BOOK.dinoIdle;
     FLIP_BOOK_WALK = this.FLIP_BOOK.dinoWalk;
@@ -25,6 +27,7 @@ class Enemy extends MoveableObject {
         this.loadImages(this.FLIP_BOOK_IDLE);
         this.loadImages(this.FLIP_BOOK_WALK);
         this.animate();
+
         // this.animate();
         // this.applyGravity();
         // this.isOnTile();
@@ -50,6 +53,7 @@ class Enemy extends MoveableObject {
         }, 1000 / 60);
 
         setInterval(() => {
+            // this.playAnimation(this.FLIP_BOOK_ATTACK);
             if (this.isBeated) {
                 this.loadImage('img/enemies/dino/Death/death6.png');
             } else if (this.hitPoints < 1 && !this.isBeated) {
