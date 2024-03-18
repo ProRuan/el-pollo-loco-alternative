0b3e4878d3c276299e55175a3d8590ff534c64bc
Your Name <you@example.com>
2024-03-18 06:44:29 +0100

    reviewed class Key

--- models/key.class.js
+++ models/key.class.js
@@ -1,14 +1,23 @@
 class Key {
     code;
-    keydown;
-    timeStamp;
-    doubleClick;
+    keydown = false;
+    timeStamp = new Date().getTime();
+    doubleClick = false;
 
 
     constructor(code) {
         this.code = code;
-        this.keydown = false;
-        this.timeStamp = new Date().getTime();
-        this.doubleClick = false;
+    }
+
+
+    get doubleClick() {
+        let currentTimeStamp = new Date().getTime();
+        this.doubleClick = (currentTimeStamp - this.timeStamp < 500) ? true : false;
+        return this.doubleClick;
+    }
+
+
+    set timeStamp(logical) {
+        this.timeStamp = (logical) ? new Date().getTime() : this.timeStamp;
     }
 }
