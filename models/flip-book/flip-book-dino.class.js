419d90ecdb7fa82c716917389a73e29a2b2a4182
Your Name <you@example.com>
2024-03-19 14:12:25 +0100

    reviewed image folder

--- models/flip-book/flip-book-dino.class.js
+++ models/flip-book/flip-book-dino.class.js
@@ -2,7 +2,7 @@ class FlipBookDino extends FlipBook {
     constructor() {
         super();
         this.path += '/enemies/dino';
-        this.cover += '/enemies/dino/Idle/idle1.png'
+        this.cover += '/enemies/dino/dino.png'
         this.attack = this.createChapter('Attack', 5);
         this.death = this.createChapter('Death', 6);
         this.hurt = this.createChapter('Hurt', 4);
