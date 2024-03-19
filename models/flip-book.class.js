f81f39e14c8e9cc263dd3873e330517b25544db2
Your Name <you@example.com>
2024-03-19 14:01:31 +0100

    reset flip book

--- models/flip-book.class.js
+++ models/flip-book.class.js
@@ -3,32 +3,6 @@ class FlipBook {
     cover = 'img';
 
 
-    // constructor(source) {
-    //     async (source) => {
-    //         let response = await fetch('./json/img-source-hero.json');
-    //         let imageSource = await response.json();
-    //         this.createFlipBook(imageSource);
-    //     }
-    // }
-
-
-    // async loadImageSource() {
-    //     let response = await fetch('./json/img-source-hero.json');
-    //     let source = await response.json();
-    //     this.createFlipBook(source);
-    // }
-
-
-    // createFlipBook(source) {
-    //     for (let i = 0; i < source.length; i++) {
-    //         let folder = source[i].folder;
-    //         let files = source[i].files;
-    //         let chapter = folder.toLowerCase();
-    //         this[chapter] = this.createChapter(folder, files);
-    //     }
-    // }
-
-
     createChapter(folder, files) {
         let images = [];
         for (let i = 0; i < files; i++) {
