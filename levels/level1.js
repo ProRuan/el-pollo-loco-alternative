d5b6e7f09531cc4340465f6174c0f6d1f7da3a15
Your Name <you@example.com>
2024-03-20 22:37:59 +0100

    created level1 grass

--- levels/level1.js
+++ levels/level1.js
@@ -1,19 +1,52 @@
 const GRASS = [];
 
-
-let section1 = new Section();
-section1.pushTile('grass-center', 0, 15);
-
-
-// let section2 = new Section();
-// section2.pushGrassCenterEnd(0, 7);
-// section2.pushGrassStartCenter(8, 7);
+let section0 = new Section(0);
+let section1 = new Section(1);
+let section2 = new Section(2);
+let section3 = new Section(3);
+let section4 = new Section(4);
 
 
-// let section3 = new Section();
-// section3.pushGrassCenterEnd(0, 5);
-// section3.pushGrassStartCenterEnd(6, 3);
-// section3.pushGrassStartCenter(10, 5);
+initLevel();
+
+
+function initLevel() {
+   loadSection0();
+   loadSection1();
+   loadSection2();
+   loadSection3();
+   loadSection4();
+}
+
+
+function loadSection0() {
+    section0.pushTile('grass-center', 0, 15);
+}
+
+
+function loadSection1() {
+    section1.pushGrassBlockEnd(0, 7);
+    section1.pushGrassBlockStart(8, 7);
+}
+
+
+function loadSection2() {
+    section2.pushGrassBlockEnd(0, 4);
+    section2.pushGrassBlockCenter(5, 5);
+    section2.pushGrassBlockStart(11, 4);
+}
+
+
+function loadSection3() {
+    section3.pushGrassBlockEnd(0, 8);
+    section3.pushGrassBlockStart(10, 5);
+}
+
+
+function loadSection4() {
+    section4.pushGrassBlockEnd(0, 5);
+    section4.pushGrassBlockStart(10, 5);
+}
 
 
 // let section4 = new Section(0);
--- models/section.class.js
+++ models/section.class.js
@@ -1,30 +1,42 @@
 class Section {
-    translation = 0;
+    translation = 960;
+
+
+    constructor(i) {
+        this.translation = (i) ? i * 960 : 0;
+    }
 
 
     pushTile(type, xStart, n) {
         for (let i = xStart; i < xStart + n; i++) {
-            let tile = new Tile(type, i + this.translation);
+            ;
+            let tile = new Tile(type, i);
+            this.applyTranslation(tile);
             GRASS.push(tile);
         }
     }
 
 
-    pushGrassStartCenter(xStart, n) {
-        this.pushTile('grass-start', xStart + this.translation, 1);
-        this.pushTile('grass-center', xStart + 1 + this.translation, n - 1);
+    applyTranslation(tile) {
+        tile.x += this.translation;
     }
 
 
-    pushGrassStartCenterEnd(xStart, n) {
-        this.pushTile('grass-start', xStart + this.translation, 1);
-        this.pushTile('grass-center', xStart + 1 + this.translation, n - 2);
-        this.pushTile('grass-end', xStart + n - 1 + this.translation, 1);
+    pushGrassBlockStart(xStart, n) {
+        this.pushTile('grass-start', xStart, 1);
+        this.pushTile('grass-center', xStart + 1, n - 1);
     }
 
 
-    pushGrassCenterEnd(xStart, n) {
-        this.pushTile('grass-center', xStart + this.translation, n - 1);
-        this.pushTile('grass-end', xStart + n - 1 + this.translation, 1);
+    pushGrassBlockCenter(xStart, n) {
+        this.pushTile('grass-start', xStart, 1);
+        this.pushTile('grass-center', xStart + 1, n - 2);
+        this.pushTile('grass-end', xStart + n - 1, 1);
+    }
+
+
+    pushGrassBlockEnd(xStart, n) {
+        this.pushTile('grass-center', xStart, n - 1);
+        this.pushTile('grass-end', xStart + n - 1, 1);
     }
 }
