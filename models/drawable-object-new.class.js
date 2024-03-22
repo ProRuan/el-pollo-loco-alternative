45f71ef20aa5e629d1bc78d6487fec1fae7e465e
Your Name <you@example.com>
2024-03-22 09:39:14 +0100

    prepared class DrawableObjectNew

--- models/drawable-object-new.class.js
+++ models/drawable-object-new.class.js
@@ -5,6 +5,8 @@ class DrawableObjectNew extends CoordinateSystem {
     imageCache = [];
     patternFile = /[a-z]+\_?[a-z]*\d+.png/;
 
+    currentImage = 0;
+
 
     constructor() {
         super();
@@ -46,5 +48,51 @@ class DrawableObjectNew extends CoordinateSystem {
             let file = chapter.match(this.patternFile);
             this.imageCache[file] = img;
         });
+    }
+
+
+    draw(ctx) {
+        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+    }
+
+
+
+
+    drawFrame(ctx) {
+        if (this instanceof Character) {
+            // body + center line
+            ctx.beginPath();
+            ctx.lineWidth = '2';
+            ctx.strokeStyle = 'blue';
+            // ctx.rect(this.x + 44, this.y + 60, 0, 48);    // centerLine: x + 44, y + 60, 32, 48
+            ctx.rect(this.x + 28, this.y + 60, 32, 48);    // border: x + 28, y + 60, 32, 48
+            ctx.stroke();
+
+            // attack range
+            ctx.beginPath();
+            ctx.lineWidth = '2';
+            ctx.strokeStyle = 'red';
+            ctx.rect(this.x + 60, this.y + 60, 48, 48);    // attack: x + 60, y + 60, 48, 48
+            // ctx.rect(this.x + 60, this.y + 60, 36, 48);    // walk atttack, run attack: x + 60, y + 60, 36, 48
+            // ctx.rect(this.x + 60, this.y + 32, 48, 80);    // attack: x + 60, y + 60, 48, 48
+            ctx.stroke();
+        }
+
+        if (this instanceof Enemy) {
+            ctx.beginPath();
+            ctx.lineWidth = '2';
+            ctx.strokeStyle = 'purple';
+            // ctx.rect(this.x, this.y + 40, 104, 48);    // x, y + 40, 104, 48 --> xCenter = 52
+            ctx.rect(this.x + 84, this.y + 40, 40, 40);    // x + 84, y + 40, 40, 40
+            ctx.stroke();
+        }
+
+        if (this instanceof AnimatedObject) {
+            ctx.beginPath();
+            ctx.lineWidth = '2';
+            ctx.strokeStyle = 'green';
+            ctx.rect(this.x + 16, this.y + 16, 32, 32);
+            ctx.stroke();
+        }
     }
 }
